3cc4a72047e7cc327597e1a8d508d3e6476adbe5
SHRI KRISHNA <rahulk.verma93@gmail.com>
2019-08-17 15:47:55 +0530

    Test Commit

--- src/automationFramework/FirstTestCase.java
+++ src/automationFramework/FirstTestCase.java
@@ -15,6 +15,7 @@ public class FirstTestCase {
 		driver.get("http://toolsqa.com/automation-practice-form/");
 		driver.findElement(By.xpath("//*[@name='firstname']")).sendKeys("Test");;
 		driver.findElement(By.xpath("//*[@name='lastname']")).sendKeys("GIT");;
+		driver.findElement(By.cssSelector("['btn']")).click();
 		System.out.println(driver.getTitle());
 		
 		driver.quit();
