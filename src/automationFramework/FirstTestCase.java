4eccfae77f2f77e8742898f8a05dc8c99ca82fbc
SHRI KRISHNA <rahulk.verma93@gmail.com>
2019-08-17 14:35:39 +0530

    First Commit

---
+++ src/automationFramework/FirstTestCase.java
@@ -0,0 +1,23 @@
+package automationFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://toolsqa.com/automation-practice-form/");
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("Test");;
+		
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+	}
+
+}
